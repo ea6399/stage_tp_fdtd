a5682e566e218f9309b669d9f6ea74b70d07a27c
ALI Emin <eminali2430@gmail.com>
2025-04-14 16:26:31 +0200

    Initialise init ainsi que les variables du problème

--- main.f90
+++ main.f90
@@ -4,9 +4,29 @@ PROGRAM FDTD_1D
 
     IMPLICIT NONE
     ! Déclaration des variables
-  
+    REAL(8) :: fmax, c
+    REAL(8) :: t0, T
+    REAL(8) :: dt, dx
     ! Classe FDTD1D
     type(FDTD1D) :: fd
+
+    !Initialisation des variables
+     
+    fmax = 1.0e9                                        ! Fréquence max d'étude
+    c = 1.d0 / sqrt(epsilon_0 * mu_0)                   ! Vitesse de la lumière
+
+    ! Calcul des pas spatio-temporels
+    dx = (c / fmax) / 30                                ! 30 cellules pour la longueur d'onde min
+    dt = 0.98d0 * dx / c                                ! Critère de stabilité dt <= dx/c
+    
+    ! Calcul de la fonction d'excitation : une gaussienne
+    T = sqrt( log(10.0d0) ) / (PI * fmax)               !
+    t0 = T * sqrt(log(1000.0d0))                        ! Retards
+
+
+
+
+
 
 
 
@@ -16,7 +36,9 @@ PROGRAM FDTD_1D
 
     ! Initialisation des résultats
     call resultat_init(fd, Nx, Nt)
-    WRITE(*,'(/,T5,A,/)') "Initialisation des résultats terminée"
+    
+    ! Initialisation des champs
+    call init(fd, Nx, dt, dx)
 
     
 
--- structure.f90
+++ structure.f90
@@ -12,45 +12,74 @@ module structure
 
     ! Type FDTD1D 
     type :: FDTD1D
-        ! FDTD variables
-        REAL(8), ALLOCATABLE :: E(:), H(:)                                          ! Champs E et H
-        REAL(8), ALLOCATABLE :: c_E(:), c_H(:)                                      ! Coefficient E et H
-        INTEGER :: Nres                                                             ! Nombre de résultats
-        INTEGER, ALLOCATABLE :: pres(:)                                                             ! Position du point d'observation pour chaque résultat
-        REAL(8), ALLOCATABLE :: Eres(:,:), Hres(:,:)                                ! Tableau 2D hébergeant les résultats E et H
+          ! FDTD variables
+          REAL(8), ALLOCATABLE :: E(:), H(:)                                          ! Champs E et H
+          REAL(8), ALLOCATABLE :: c_E(:), c_H(:)                                      ! Coefficient E et H
+          INTEGER :: Nres                                                             ! Nombre de résultats
+          INTEGER, ALLOCATABLE :: pres(:)                                                             ! Position du point d'observation pour chaque résultat
+          REAL(8), ALLOCATABLE :: Eres(:,:), Hres(:,:)                                ! Tableau 2D hébergeant les résultats E et H
     CONTAINS    
-        procedure :: resultat_init
-        ! procedure :: init 
-        ! procedure :: calcule
-        ! procedure :: resultat_stockage
+          procedure :: resultat_init
+          procedure :: init 
+          ! procedure :: calcule
+          ! procedure :: resultat_stockage
     end type FDTD1D
 
 
     CONTAINS
 
 
+
+                    !----------------------------------------!
+                    ! Definition des points d'observation    !
+                    !----------------------------------------!
     SUBROUTINE resultat_init(fd, Nx, Nt)
-        ! Initialisation des arguments
-        class(FDTD1D), intent(inout) :: fd
-        INTEGER, intent(in) :: Nx, Nt
+          ! Initialisation des arguments
+          IMPLICIT NONE
+          class(FDTD1D), intent(inout) :: fd
+          INTEGER, intent(in) :: Nx, Nt
 
-        fd%Nres = 4 ! Nombre de résultats
+          fd%Nres = 4 ! Nombre de résultats
 
-        ALLOCATE(fd%pres(1 : fd%Nres)) ! Allocation de la mémoire pour le tableau de positions
-        ALLOCATE(fd%Eres(Nt,fd%Nres)) ! Allocation de la mémoire pour le tableau de résultats E, champs éléctrique
-        ALLOCATE(fd%Hres(Nt,fd%Nres)) ! Allocation de la mémoire pour le tableau de résultats H, champs magnétique
+          ALLOCATE(fd%pres(1 : fd%Nres)) ! Allocation de la mémoire pour le tableau de positions
+          ALLOCATE(fd%Eres(Nt,fd%Nres)) ! Allocation de la mémoire pour le tableau de résultats E, champs éléctrique
+          ALLOCATE(fd%Hres(Nt,fd%Nres)) ! Allocation de la mémoire pour le tableau de résultats H, champs magnétique
 
-        ! Initialisation des positions de résultats
-        fd%pres(1) = 1
-        fd%pres(2) = 100
-        fd%pres(3) = 200
-        fd%pres(4) = Nx
+          ! Initialisation des positions de résultats
+          fd%pres(1) = 1
+          fd%pres(2) = 100
+          fd%pres(3) = 200
+          fd%pres(4) = Nx
 
-        ! Initialisation des tableaux de résultats
-        fd%Eres = 0.0d0
-        fd%Hres = 0.0d0
+          ! Initialisation des tableaux de résultats
+          fd%Eres = 0.0d0
+          fd%Hres = 0.0d0
 
+          WRITE(*,'(/,T5,A,/)') "Initialisation des points d'observation terminée."
     END SUBROUTINE resultat_init  
+
+
+
+                    !------------------------------------------------!
+                    ! Allocation des tableaux et calcul coeff champ  !
+                    !------------------------------------------------!
+    SUBROUTINE init(fd, Nx, dt, dx)
+          IMPLICIT NONE
+          class(FDTD1D), intent(inout) :: fd
+          INTEGER, intent(in) :: Nx
+          REAL(8), intent(in) :: dt, dx
+
+          ALLOCATE( fd%E(Nx + 1), fd%H(Nx + 1), fd%c_E(Nx + 1), fd%c_H(Nx + 1)) ! Allocation de la mémoire pour les champs E et H
+          WRITE(*,'(/,T5,A,ES10.3,/)') "dt = ", dt
+
+          ! Initialiation des champs E et H ainsi que des coefficients
+          fd%E = 0.0d0
+          fd%H = 0.0d0
+          fd%c_E = dt / (epsilon_0 * dx)
+          fd%c_H = dt / (mu_0 * dx)
+
+          WRITE(*,'(/,T5,A,/)') "Initialisation des champs ainsi que des coefficents terminée."
+    ENDSUBROUTINE init
 
 
 
