b1363b98a137a5a22f5b15fb60b1a461a602b82a
ALI Emin <eminali2430@gmail.com>
2025-04-14 15:53:29 +0200

    Initialise les variables résultats

--- main.f90
+++ main.f90
@@ -3,12 +3,20 @@ PROGRAM FDTD_1D
     use source
 
     IMPLICIT NONE
+    ! Déclaration des variables
+  
+    ! Classe FDTD1D
+    type(FDTD1D) :: fd
 
 
 
     !----------------------------------------!
     ! 1D FDTD simulation of a Gaussian pulse !
     !----------------------------------------!
+
+    ! Initialisation des résultats
+    call resultat_init(fd, Nx, Nt)
+    WRITE(*,'(/,T5,A,/)') "Initialisation des résultats terminée"
 
     
 
--- structure.f90
+++ structure.f90
@@ -33,33 +33,22 @@ module structure
         ! Initialisation des arguments
         class(FDTD1D), intent(inout) :: fd
         INTEGER, intent(in) :: Nx, Nt
-        
-        ! Initialisation des variables locales
-        INTEGER :: i, n
 
         fd%Nres = 4 ! Nombre de résultats
 
-        ALLOCATE(fd%pres(fd%Nres)) ! Allocation de la mémoire pour le tableau de positions
+        ALLOCATE(fd%pres(1 : fd%Nres)) ! Allocation de la mémoire pour le tableau de positions
         ALLOCATE(fd%Eres(Nt,fd%Nres)) ! Allocation de la mémoire pour le tableau de résultats E, champs éléctrique
         ALLOCATE(fd%Hres(Nt,fd%Nres)) ! Allocation de la mémoire pour le tableau de résultats H, champs magnétique
 
+        ! Initialisation des positions de résultats
         fd%pres(1) = 1
         fd%pres(2) = 100
         fd%pres(3) = 200
         fd%pres(4) = Nx
 
-        
-
-
-        fd%E = 0.0d0
-        fd%H = 0.0d0
-
-        fd%c_E = 1.0d0 / (epsilon_0 * fd%c_H)
-        fd%c_H = 1.0d0 / (mu_0 * fd%c_E)
-
-        fd%Nres = 10
-        fd%pres = 100
-
+        ! Initialisation des tableaux de résultats
+        fd%Eres = 0.0d0
+        fd%Hres = 0.0d0
 
     END SUBROUTINE resultat_init  
 
