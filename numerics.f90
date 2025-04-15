5c8b42553bbfe59a18b6b63379bc5fbc72396d08
ALI Emin <eminali2430@gmail.com>
2025-04-15 10:47:13 +0200

    Déclaration des paramètres

--- numerics.f90
+++ numerics.f90
@@ -1,49 +1,25 @@
-module numerics
-    implicit none
+Module numerics
+! Déclaration de variables à portée globale
+      IMPLICIT NONE 
+            INTEGER, PARAMETER :: Nt = 1200                                             ! Nombre d'échantillons en temps
+            INTEGER, PARAMETER :: Nx = 500                                              ! Nombre d'échantillons en espace
+            REAL(8), PARAMETER :: epsilon_0 = 8.854187817e-12                           ! Permittivité du vide
+            REAL(8), PARAMETER :: mu_0 = 1.256637061e-7                                 ! Perméabilité du vide
+            REAL(8), PARAMETER :: PI = atan(1.0d0) * 4.0d0                              ! Constante pi
 
-    ! Déclaration de variables
-    INTEGER, PARAMETER :: Nt = 1200                                             ! Nombre d'échantillons en temps
-    INTEGER, PARAMETER :: Nx = 500                                              ! Nombre d'échantillons en espace
-    REAL(8), PARAMETER :: epsilon_0 = 8.854187817e-12                           ! Permittivité du vide
-    REAL(8), PARAMETER :: mu_0 = 1.256637061e-7                                 ! Perméabilité du vide
-    REAL(8), PARAMETER :: PI = atan(1.0d0) * 4.0d0                              ! Constante pi
-
-
-
-    ! FDTD variables
-    REAL(8), ALLOCATABLE :: E(:), H(:)                                          ! Champs E et H
-    REAL(8), ALLOCATABLE :: c_E(:), c_H(:)                                      ! Coefficient E et H
-    INTEGER :: Nres                                                             ! Nombre de résultats
-    INTEGER :: pres                                                             ! Position du point d'observation pour chaque résultat
-    REAL(8), ALLOCATABLE :: Eres(:,:), Hres(:,:)                                ! Tableau 2D hébergeant les résultats E et H
-
-
-
-
-    CONTAINS
+            ! Déclaration des variables
+            REAL(8), PARAMETER :: fmax     = 1.0d9
+            REAL(8), PARAMETER :: c        = 1.0d0 / sqrt(epsilon_0 * mu_0)
+            REAL(8), PARAMETER :: attfmax  = 10.0d0
+            REAL(8), PARAMETER :: att0     = 1000.0d0
+            REAL(8), PARAMETER :: a0       = 1.0d0
+            REAL(8), PARAMETER :: T        = sqrt(log(attfmax)) / (PI * fmax)
+            REAL(8), PARAMETER :: t0       = T * sqrt(log(att0))
+            REAL(8), PARAMETER :: dx       = (c / fmax) / 30.0d0
+            REAL(8), PARAMETER :: dt       = 0.98d0 * dx / c
+            INTEGER, PARAMETER :: LBD      = 0
+            INTEGER, PARAMETER :: UBD      = Nt - 1
 
 
-    SUBROUTINE resultat_init(Nx,dt,dx)
-        ! Initialisation des variables
-        INTEGER :: Nx
-        REAL(8) :: dt, dx
-
-        ALLOCATE( E(Nx + 1), H(Nx + 1), c_E(Nx + 1), c_H(Nx + 1) )
-
-        Nres = 4
-
-        E = 0.0d0
-        H = 0.0d0
-
-        c_E = 1.0d0 / (epsilon_0 * c_H)
-        c_H = 1.0d0 / (mu_0 * c_E)
-
-        Nres = 10
-        pres = 100
-
-
-    END SUBROUTINE resultat_init  
-
-
-
-endmodule numerics
+      CONTAINS
+end module numerics
