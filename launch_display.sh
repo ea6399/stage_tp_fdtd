906b329d14d191f8cec82c3db9912b6ae5787e1a
ALI Emin <eminali2430@gmail.com>
2025-04-16 11:54:46 +0200

    Ajout d'observateur

--- launch_display.sh
+++ launch_display.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-make && ./exec && gnuplot display_fdtd1.gp && gnuplot display_fdtd2.gp && gnuplot display_fdtd3.gp && gnuplot display_fdtd4.gp && gnuplot display_fdtd5.gp &&
-gnuplot display_fdtd6.gp && make clean
+make && 
+./exec &&
+gnuplot display_fdtd1.gp && 
+gnuplot display_fdtd2.gp && 
+gnuplot display_fdtd3.gp &&
+gnuplot display_fdtd4.gp &&
+gnuplot display_fdtd5.gp &&
+gnuplot display_fdtd6.gp &&
+gnuplot display_fdtd7.gp &&
+gnuplot display_fdtd8.gp &&
+gnuplot display_fdtd8.gp &&
+gnuplot display_fdtd9.gp &&
+gnuplot display_fdtd10.gp &&
+  make clean
