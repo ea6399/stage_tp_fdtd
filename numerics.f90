e5c51f66a41ef594ccd039f84480982cf29b5cea
ALI Emin <eminali2430@gmail.com>
2025-06-17 15:28:40 +0200

    update

--- numerics.f90
+++ numerics.f90
@@ -15,7 +15,7 @@ Module numerics
             REAL(8), PARAMETER :: a0       = 1.0d0
             REAL(8), PARAMETER :: T        = sqrt(log(attfmax)) / (PI * fmax)
             REAL(8), PARAMETER :: t0       = T * sqrt(log(att0))
-            REAL(8), PARAMETER :: mesh_density = 5.0d0                                  ! Densité du maillage spatial
+            REAL(8), PARAMETER :: mesh_density = 30.0d0                                  ! Densité du maillage spatial
             REAL(8), PARAMETER :: dx       = (c / fmax) / mesh_density
             CHARACTER(LEN = 20) :: namefile = "E_5"
             REAL(8), PARAMETER :: dt       = 0.98d0 * dx / c
