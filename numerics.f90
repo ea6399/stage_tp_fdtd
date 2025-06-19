233eab842eaddf3a5c1fe9012a478284baefafb4
ALI Emin <eminali2430@gmail.com>
2025-06-19 16:45:26 +0200

    uate

--- numerics.f90
+++ numerics.f90
@@ -4,7 +4,7 @@ Module numerics
             INTEGER, PARAMETER :: Nt = 1000                                              ! Nombre d'échantillons en temps
             INTEGER, PARAMETER :: Nx = 499                                              ! Nombre d'échantillons en espace
             REAL(8), PARAMETER :: epsilon_0 = 8.854187817e-12                           ! Permittivité du vide
-            REAL(8), PARAMETER :: mu_0 = 1.256637061e-7                                ! Perméabilité du vide
+            REAL(8), PARAMETER :: mu_0 = 12.56637061e-7                                ! Perméabilité du vide
             REAL(8), PARAMETER :: PI = atan(1.0d0) * 4.0d0                              ! Constante pi
 
             ! Déclaration des variables
@@ -15,7 +15,7 @@ Module numerics
             REAL(8), PARAMETER :: a0       = 1.0d0
             REAL(8), PARAMETER :: T        = sqrt(log(attfmax)) / (PI * fmax)
             REAL(8), PARAMETER :: t0       = T * sqrt(log(att0))
-            REAL(8), PARAMETER :: mesh_density = 50.d0                                  ! Densité du maillage spatial
+            REAL(8), PARAMETER :: mesh_density = 100.d0                                  ! Densité du maillage spatial
             REAL(8), PARAMETER :: dx       = (c / fmax) / mesh_density
             REAL(8), PARAMETER :: dt       = 0.98d0 * dx / c
             INTEGER, PARAMETER :: LBD      = 0
@@ -25,8 +25,8 @@ Module numerics
 
 
                   ! Result Saving
-            CHARACTER(LEN = 20), PARAMETER :: nameE = "E_4"
-            CHARACTER(LEN = 20), PARAMETER :: nameH = "H_4"
+            CHARACTER(LEN = 20), PARAMETER :: nameE = "E_6"
+            CHARACTER(LEN = 20), PARAMETER :: nameH = "H_6"
             CHARACTER(LEN=:),allocatable :: fnameE, fnameH
 
 
