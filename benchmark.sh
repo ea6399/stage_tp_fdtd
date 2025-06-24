2de0fb7255cc0c657842a7189602078b106519df
ALI Emin <eminali2430@gmail.com>
2025-06-24 14:41:26 +0200

    Update name

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-exe="./exec"    # remplacez par le chemin de votre binaire
-outfile="times.csv"  # résultat en CSV pour Excel/gnuplot…
+exe="./bin/exec"    # remplacez par le chemin de votre binaire
+outfile="times_fd.csv"  # résultat en CSV pour Excel/gnuplot…
 nruns=100             # nombre d’itérations
 
 echo "run,elapsed_s" > $outfile
